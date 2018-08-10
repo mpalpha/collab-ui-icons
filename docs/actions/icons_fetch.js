acc29936efa7fc8eae6be546b446f1e9e4653dab
mpalpha (Jason Lusk) <jason@jasonlusk.com>
2018-08-10 11:37:09 -0500

    fix alignment issues and add json file

--- docs/actions/icons_fetch.js
+++ docs/actions/icons_fetch.js
@@ -1,25 +1,27 @@
 import * as types from '../constants';
 
-const iconsURL = 'http://collab-ui-icons.cisco.com:8080/api/icons';
+// const iconsURL = 'http://collab-ui-icons.cisco.com:8080/api/icons'; // JL: comment out api
+
+import iconsJson from '../icons'; // JL: load json file instead of api
 
 const setIcons = icons => {
-  return {type: types.FETCH_ICONS_SUCCESS, icons};
+  return { type: types.FETCH_ICONS_SUCCESS, icons };
 };
 
 const setError = error => {
-  return {type: types.FETCH_ICONS_ERROR, error};
+  return { type: types.FETCH_ICONS_ERROR, error };
 };
 
-const compare = (a,b) => {
+const compare = (a, b) => {
   return a.name > b.name ? 1 : -1;
 };
 
-const fetchIcons = () => async (dispatch) => {
-  dispatch({type: types.FETCH_ICONS_PENDING });
-  
+const fetchIcons = () => async dispatch => {
+  dispatch({ type: types.FETCH_ICONS_PENDING });
+
   try {
-    const fetchedIcons = await fetch(iconsURL, { method: 'GET'});
-    const fetchedIconsToJSON = await fetchedIcons.json();
+    // const fetchedIcons = await fetch(iconsURL, { method: 'GET' });   // JL: disable api data
+    const fetchedIconsToJSON = iconsJson; //await fetchedIcons.json();  // JL: use loaded json file instead of api
     const sortedIcons = fetchedIconsToJSON.sort(compare);
 
     return dispatch(setIcons(sortedIcons));
